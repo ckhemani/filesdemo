d2f199c0d1e20d07914ad8b75ccce2652d5a7c3c
ckhemani <chandru.khemani@gmail.com>
2022-03-31 14:05:21 -0400

    update methods for creating,reading,writing and deleting files

--- workingwithfiles/src/main/java/Main.java
+++ workingwithfiles/src/main/java/Main.java
@@ -1,6 +1,69 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private static final Path filepath = Paths.get(System.getProperty("user.dir") + "\\resources\\sample_data.txt");
+    public static void main(String[] args) throws IOException {
         String welcome = "Welcome to Files";
         System.out.println(welcome);
+
+        //Create
+        System.out.println("\nCalling the create a file method");
+        createAFile();
+
+        //Update
+        System.out.println("\n Calling the Update Method.");
+        writeAFile();
+
+        //Read
+        System.out.println("\n Calling the Read Method");
+        readAFile();
+
+        //Delete
+        System.out.println("\n Calling the Delete Method");
+        deleteAFile();
+    }
+
+    //Create
+    public static void createAFile(){
+        try{
+            Files.createFile(filepath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //Read
+    public static void writeAFile(){
+        try{
+            Files.writeString(filepath,"This is the end my Friend..");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Update
+    public static void readAFile(){
+        String temp;
+        try{
+            temp = Files.readString(filepath);
+            System.out.println(temp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //Delete
+//    public static void deleteAFile(){
+//        try{
+//            Files.delete(filepath);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+    //Delete
+    public static void deleteAFile() throws IOException {
+        Files.delete(filepath);
     }
 }
